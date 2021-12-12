66c4631a4f49c9068815927e9deb25fc024c2fbf
Péter Jeszenszky <jeszenszky.peter.deik@gmail.com>
2021-12-12 16:09:42 +0100

    Marked the annotation interface Test with the @Documented annotation

--- src/main/java/uunit/Test.java
+++ src/main/java/uunit/Test.java
@@ -1,5 +1,6 @@
 package uunit;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,6 +9,7 @@ import java.lang.annotation.Target;
 /**
  * Indicates that the annotated method is intended to be a test method.
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Test {
